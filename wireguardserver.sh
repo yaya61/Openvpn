ebb5821c0ad1b6c812170fa03fb752efb1c3a2b7
yaya61 <105031206+yaya61@users.noreply.github.com>
2025-01-27 01:35:17 +0000

    wireguardserver.sh

--- wireguardserver.sh
+++ wireguardserver.sh
@@ -26,30 +26,3 @@ sudo systemctl enable wg-quick@wg0
 sudo systemctl start wg-quick@wg0
 
 
-
-sudo nano /etc/wireguard/wg0.conf
-
-[Interface]
-Address = 10.0.0.2/32
-PrivateKey = <CLIENT_PRIVATE_KEY>
-DNS = 8.8.8.8
-
-[Peer]
-PublicKey = <SERVER_PUBLIC_KEY>
-Endpoint = <SERVER_IP>:51820
-AllowedIPs = 0.0.0.0/0
-PersistentKeepalive = 25
-[Interface]
-Address = 10.0.0.2/32
-PrivateKey = <CLIENT_PRIVATE_KEY>
-DNS = 8.8.8.8
-
-[Peer]
-PublicKey = <SERVER_PUBLIC_KEY>
-Endpoint = <SERVER_IP>:51820
-AllowedIPs = 0.0.0.0/0
-PersistentKeepalive = 25
-
-
-sudo systemctl enable wg-quick@wg0
-sudo systemctl start wg-quick@wg0
